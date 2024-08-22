69195e336e4631232bf1ed94da82a7488bb85574
Mashrafie <rahimsheikh05@gmail.com>
2024-08-23 00:49:32 +0600

    Removed logging...

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,8 +59,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 const createBookingCheckout = async (session, req, res) => {
-  console.log(session);
-
   const tour = session.client_reference_id;
   const user = await User.findOne({ email: session.customer_email });
   const price = session.amount_subtotal / 100;
