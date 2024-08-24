a970667878f1d09dfeee7e1d622dcdb0422cf53d
Mashrafie <rahimsheikh05@gmail.com>
2024-08-24 11:09:09 +0600

    nothing special...

--- public/js/index.js
+++ public/js/index.js
@@ -126,6 +126,5 @@ if (resetPasswordBtn) {
 const alertMessage = document.querySelector('body').dataset.alert;
 
 if (alertMessage) {
-  console.log('hii');
   showAlert('success', alertMessage);
 }
